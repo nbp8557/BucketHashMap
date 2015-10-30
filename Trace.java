80b02928983b7457e1931be3d32ac119642dfe65
nbp8557 <nbp8557@rit.edu>
2015-10-30 19:38:09 -0400

    Working on integrating better testing. But not sure how to test the hashmap concurrency atm still

--- Trace.java
+++ Trace.java
@@ -24,9 +24,14 @@ public class Trace {
         }
 
         public void run() {
-            this.hashMap.put("Test1",3);
-            this.hashMap.put("Test2",2);
-            this.hashMap.put("Test3",3);
+            java.util.Random rand = new java.util.Random();
+            float randomNum = 0;
+
+            for(int counter=0; counter<5; counter++){
+                randomNum = rand.nextFloat();
+                this.hashMap.put("Test"+randomNum,counter);
+                System.out.println("Added new Key: Test" + randomNum + " with a value of: " + counter);
+            }
         }
     }
 
